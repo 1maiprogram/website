01213dbbcd3ec1d510c939499a7013e0d22e5d1d
Håkon Løvdal <kode@denkule.no>
2025-04-01 02:08:07 +0200

    Format files

--- src/app/selector/fylke-selector/fylke-selector.component.spec.ts
+++ src/app/selector/fylke-selector/fylke-selector.component.spec.ts
@@ -2,26 +2,25 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { FylkeSelectorComponent } from './fylke-selector.component';
+import { FylkeSelectorComponent } from "./fylke-selector.component";
 
-describe('FylkeSelectorComponent', () => {
-  let component: FylkeSelectorComponent;
-  let fixture: ComponentFixture<FylkeSelectorComponent>;
+describe("FylkeSelectorComponent", () => {
+    let component: FylkeSelectorComponent;
+    let fixture: ComponentFixture<FylkeSelectorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FylkeSelectorComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [FylkeSelectorComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(FylkeSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(FylkeSelectorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/selector/fylke-selector/fylke-selector.component.ts
+++ src/app/selector/fylke-selector/fylke-selector.component.ts
@@ -2,13 +2,13 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-fylke-selector',
-  imports: [],
-  templateUrl: './fylke-selector.component.html',
-  styleUrl: './fylke-selector.component.scss'
+    selector: "app-fylke-selector",
+    imports: [],
+    templateUrl: "./fylke-selector.component.html",
+    styleUrl: "./fylke-selector.component.scss",
 })
 export class FylkeSelectorComponent {
 
