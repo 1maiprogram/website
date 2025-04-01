15ca8e56edbe244409d4680cc08b4292953ca0b8
Håkon Løvdal <kode@denkule.no>
2025-04-01 02:06:56 +0200

    npx ng generate component selector/fylke-selector
    ./bin/add-spdx-to-new-files.sh

---
+++ src/app/selector/fylke-selector/fylke-selector.component.spec.ts
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FylkeSelectorComponent } from './fylke-selector.component';
+
+describe('FylkeSelectorComponent', () => {
+  let component: FylkeSelectorComponent;
+  let fixture: ComponentFixture<FylkeSelectorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FylkeSelectorComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FylkeSelectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/selector/fylke-selector/fylke-selector.component.ts
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fylke-selector',
+  imports: [],
+  templateUrl: './fylke-selector.component.html',
+  styleUrl: './fylke-selector.component.scss'
+})
+export class FylkeSelectorComponent {
+
+}
