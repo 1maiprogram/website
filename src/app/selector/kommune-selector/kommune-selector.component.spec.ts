4525ead52cb41ab931dbaae7c6f859e038426591
Håkon Løvdal <kode@denkule.no>
2025-04-01 22:05:38 +0200

    npx ng generate component selector/kommune-selector
    ./bin/add-spdx-to-new-files.sh

---
+++ src/app/selector/kommune-selector/kommune-selector.component.spec.ts
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KommuneSelectorComponent } from './kommune-selector.component';
+
+describe('KommuneSelectorComponent', () => {
+  let component: KommuneSelectorComponent;
+  let fixture: ComponentFixture<KommuneSelectorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [KommuneSelectorComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(KommuneSelectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/selector/kommune-selector/kommune-selector.component.ts
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kommune-selector',
+  imports: [],
+  templateUrl: './kommune-selector.component.html',
+  styleUrl: './kommune-selector.component.scss'
+})
+export class KommuneSelectorComponent {
+
+}
