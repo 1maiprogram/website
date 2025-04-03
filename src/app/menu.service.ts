5b49d74d02334da447087c0bc390343e4b263d0f
Håkon Løvdal <kode@denkule.no>
2025-04-04 01:09:56 +0200

    Implement MenuService

--- src/app/menu.service.ts
+++ src/app/menu.service.ts
@@ -2,13 +2,51 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import { Injectable } from "@angular/core";
+import { Injectable, signal, WritableSignal } from "@angular/core";
+
+export const MenuItemKeyObj = {
+    FylkeSelector: "FylkeSelector",
+    KommuneSelector: "KommuneSelector",
+    Kommune: "Kommune",
+} as const;
+export type MenuItemKey = keyof(typeof MenuItemKeyObj);
+
+export class MenuItem {
+    textSignal: WritableSignal<string>;
+    urlSignal: WritableSignal<string>;
+    visibleSignal: WritableSignal<boolean>;
+    constructor(
+        value: string,
+        url: string,
+        visible: boolean,
+    ) {
+        this.textSignal = signal(value);
+        this.urlSignal = signal(url);
+        this.visibleSignal = signal(visible);
+    }
+}
 
 @Injectable({
     providedIn: "root",
 })
 export class MenuService {
+    private menuItemsMap = new Map<string, MenuItem>();
+    private fylkeSelectorMenuItem = new MenuItem("Velg fylke", "2025", true);
+    private kommuneSelectorMenuItem = new MenuItem("Velg kommune", "", false);
+    private kommuneMenuItem = new MenuItem("", "", false);
+
     constructor(
     ) {
+        this.menuItemsMap.set(MenuItemKeyObj.FylkeSelector, this.fylkeSelectorMenuItem);
+        this.menuItemsMap.set(MenuItemKeyObj.KommuneSelector, this.kommuneSelectorMenuItem);
+        this.menuItemsMap.set(MenuItemKeyObj.Kommune, this.kommuneMenuItem);
+    }
+
+    getMenuItem(menuItemKey: MenuItemKey): MenuItem {
+        const menuItem = this.menuItemsMap.get(menuItemKey);
+        if (!menuItem) {
+            throw new Error(`No menu item found for ${menuItemKey}`);
+        }
+        return menuItem;
     }
 }
