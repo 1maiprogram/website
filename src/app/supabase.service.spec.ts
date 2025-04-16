b22e9756aefa3f3f718ecafb256cedc9c43f9b80
Håkon Løvdal <kode@denkule.no>
2025-04-16 20:53:40 +0200

    npx ng generate service supabase
    ./bin/add-spdx-to-new-files.sh

---
+++ src/app/supabase.service.spec.ts
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { TestBed } from '@angular/core/testing';
+
+import { SupabaseService } from './supabase.service';
+
+describe('SupabaseService', () => {
+  let service: SupabaseService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SupabaseService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
