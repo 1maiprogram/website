c89d0ab08cf2f6a32325b4bd25f5e9ba9b5bfebc
Håkon Løvdal <kode@denkule.no>
2025-04-18 00:00:08 +0200

    npx ng generate component shared/spinner
    ./bin/add-spdx-to-new-files.sh

---
+++ src/app/shared/spinner/spinner.component.spec.ts
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpinnerComponent } from './spinner.component';
+
+describe('SpinnerComponent', () => {
+  let component: SpinnerComponent;
+  let fixture: ComponentFixture<SpinnerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SpinnerComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SpinnerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
