4acdad37e51d00c8165878fe0f34c705a4ce9262
Håkon Løvdal <kode@denkule.no>
2025-04-19 00:47:52 +0200

    ng generate component oppdatering
    ./bin/add-spdx-to-new-files.sh

---
+++ src/app/oppdatering/oppdatering.component.spec.ts
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OppdateringComponent } from './oppdatering.component';
+
+describe('OppdateringComponent', () => {
+  let component: OppdateringComponent;
+  let fixture: ComponentFixture<OppdateringComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [OppdateringComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OppdateringComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/oppdatering/oppdatering.component.ts
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oppdatering',
+  imports: [],
+  templateUrl: './oppdatering.component.html',
+  styleUrl: './oppdatering.component.scss'
+})
+export class OppdateringComponent {
+
+}
