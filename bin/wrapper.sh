01c7c37efcb681a8a7f5742c3be9a5bfaeea8a53
Håkon Løvdal <kode@denkule.no>
2025-04-20 22:49:39 +0200

    Add test wrapper script and initial test scripts
    (cherry picked from commit 239c9f2c5156e067f2452c62d4fb5bc954d0d946)

--- bin/wrapper.sh
+++ bin/wrapper.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
-exec /bin/true
+#!/bin/bash
+
+# Wrapper script to let a test pass on commits from before a specific
+# test script was added.
+
+set -euo pipefail
+
+cd "$(git rev-parse --show-toplevel)"
+
+if [[ -x "$1" ]]
+then
+	exec "$@"
+fi
