7cf503484bfd30734ee6205caba8fb4426c5c610
Håkon Løvdal <kode@denkule.no>
2025-04-20 03:57:19 +0200

    Add footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,14 +3,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import { TestBed } from "@angular/core/testing";
+import { ActivatedRoute } from "@angular/router";
+import { createSpyFromClass, Spy } from "jasmine-auto-spies";
 
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
+    let activatedRouteSpy: Spy<ActivatedRoute>;
+
     beforeEach(async () => {
+        activatedRouteSpy = createSpyFromClass(ActivatedRoute);
+
         await TestBed.configureTestingModule({
             imports: [
                 AppComponent,
+            ],
+            providers: [
+                { provide: ActivatedRoute, useValue: activatedRouteSpy },
             ],
         }).compileComponents();
     });
