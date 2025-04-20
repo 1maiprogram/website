2263e3e0f685f5e3fb17ef9efdeff99ef3e6e1ad
Håkon Løvdal <kode@denkule.no>
2025-04-20 04:03:28 +0200

    Format files

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,13 +2,14 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+    selector: "app-about",
+    imports: [
+    ],
+    templateUrl: "./about.component.html",
+    styleUrl: "./about.component.scss",
 })
 export class AboutComponent {
 
