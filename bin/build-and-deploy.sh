a2ac2616edf5ee17ec82e816c4047a67d4850df7
Håkon Løvdal <kode@denkule.no>
2025-04-22 20:51:03 +0200

    Add deployment script

---
+++ bin/build-and-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -euo pipefail
+
+cd "$(git rev-parse --show-toplevel)"
+
+. .env.deploy
+
+rm -r dist
+npm run build
+find dist -name '*.license' -print0 | xargs -0 --no-run-if-empty rm
+
+scp -r dist/1maiprogram/browser "$DEPLOY_USER"@"$DEPLOY_SERVER":www.new
