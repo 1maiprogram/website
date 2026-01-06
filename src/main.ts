84188420fad869fc60e314615f5ae4ea04497cde
Håkon Løvdal <kode@denkule.no>
2026-01-06 23:49:45 +0100

    Revert automatic provideZoneChangeDetection change

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { provideZoneChangeDetection } from "@angular/core";
 // SPDX-FileCopyrightText: 2025 Håkon Løvdal <kode@denkule.no>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
@@ -7,6 +6,6 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 
-bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]}).catch((err) =>
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err)
 );
