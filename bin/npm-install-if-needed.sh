a4a39767eac3c81ebb42073f694bfa1f40ce8e37
Håkon Løvdal <kode@denkule.no>
2026-02-06 00:53:05 +0100

    Replace install with ci
    Will now fail on lock file being out of sync, e.g.
    
    npm error `npm ci` can only install packages when your package.json and package-lock.json or npm-shrinkwrap.json are in sync. Please update your lock file with `npm install` before continuing.

--- bin/npm-install-if-needed.sh
+++ bin/npm-install-if-needed.sh
@@ -10,11 +10,11 @@ then
 	PARENT="$(git log --pretty=%P -n 1 HEAD)"
 	if [[ "$GIT_TEST_PREVIOUS_CHECKED_OUT_COMMIT" = "$PARENT" ]]
 	then
-		# ... and package.json has not changed then skip running npm install.
-		git diff --quiet "$PARENT" -- package.json || npm install
+		# ... and package.json has not changed then skip running npm ci.
+		git diff --quiet "$PARENT" -- package.json || npm ci
 	else
-		npm install
+		npm ci
 	fi
 else
-	npm install
+	npm ci
 fi
