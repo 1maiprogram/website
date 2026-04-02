41962f10ad24da95a8d449386a20f558b0ebb1d8
Håkon Løvdal <kode@denkule.no>
2026-04-02 19:49:36 +0200

    npx ng generate component selector/year-selector
    ./bin/add-spdx-to-new-files.sh

---
+++ src/app/selector/year-selector/year-selector.component.spec.ts
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2026 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { YearSelectorComponent } from './year-selector.component';
+
+describe('YearSelectorComponent', () => {
+    let component: YearSelectorComponent;
+    let fixture: ComponentFixture<YearSelectorComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [YearSelectorComponent],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(YearSelectorComponent);
+        component = fixture.componentInstance;
+        await fixture.whenStable();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/selector/year-selector/year-selector.component.ts
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2026 Håkon Løvdal <kode@denkule.no>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-year-selector',
+    imports: [],
+    templateUrl: './year-selector.component.html',
+    styleUrl: './year-selector.component.scss',
+})
+export class YearSelectorComponent {}
