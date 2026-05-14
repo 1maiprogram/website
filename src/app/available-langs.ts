1f01927b167de0a17ae2ca3c39d6472aafd75585
Håkon Løvdal <kode@denkule.no>
2026-05-14 16:53:19 +0200

    Add flags to language selector

--- src/app/available-langs.ts
+++ src/app/available-langs.ts
@@ -7,15 +7,16 @@ export type SupportedLanuages = "no_NB" | "no_NN" | "no_SE" | "en_GB";
 export interface Language {
     iso639_1code: SupportedLanuages;
     displayName: string;
+    flagEmoji: string; // Possible source: https://emojipedia.org/flags
 }
 
 export const defaultLang = "no_NB";
 
 export const languages: Language[] = [
-    { iso639_1code: "no_NB", displayName: "Norsk, bokmål" },
-    { iso639_1code: "no_NN", displayName: "Norsk, nynorsk" },
-    { iso639_1code: "no_SE", displayName: "Davvisámegiella (Nordsamisk)" },
-    { iso639_1code: "en_GB", displayName: "English" },
+    { iso639_1code: "no_NB", displayName: "Norsk, bokmål", flagEmoji: "🇳🇴" },
+    { iso639_1code: "no_NN", displayName: "Norsk, nynorsk", flagEmoji: "🇳🇴" },
+    { iso639_1code: "no_SE", displayName: "Davvisámegiella (Nordsamisk)", flagEmoji: "🇳🇴" },
+    { iso639_1code: "en_GB", displayName: "English", flagEmoji: "🇬🇧" },
 ];
 
 export const availableLangs = languages.map((lang) => lang.iso639_1code);
