34cd1266797979df4d11459639fd467c04141469
Håkon Løvdal <kode@denkule.no>
2026-05-14 15:51:05 +0200

    Save selected language to local storage

--- src/app/selector/language-selector/language-selector.component.ts
+++ src/app/selector/language-selector/language-selector.component.ts
@@ -2,11 +2,13 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TranslocoModule, TranslocoService } from "@jsverse/transloco";
 
-import { defaultLang, languages } from "../../available-langs";
+import { availableLangs, defaultLang, languages, SupportedLanuages } from "../../available-langs";
 import { isEventTarget } from "@shared/utils";
+
+const LANG_KEY = 'language';
 
 @Component({
     selector: "app-language-selector",
@@ -16,13 +18,25 @@ import { isEventTarget } from "@shared/utils";
     templateUrl: "./language-selector.component.html",
     styleUrl: "./language-selector.component.scss",
 })
-export class LanguageSelectorComponent {
+export class LanguageSelectorComponent implements OnInit {
     selectedLanguage: string = defaultLang;
     languages = languages;
 
     constructor(
         private translocoService: TranslocoService,
     ) {
+        const savedLanguage = localStorage.getItem(LANG_KEY);
+        if (savedLanguage) {
+            if (availableLangs.includes(savedLanguage as SupportedLanuages)) {
+                this.selectedLanguage = savedLanguage;
+            } else {
+                console.warn(`Saved language ${savedLanguage} is not in available languages, falling back to default language ${defaultLang}`);
+            }
+        }
+    }
+
+    ngOnInit() {
+        this.translocoService.setActiveLang(this.selectedLanguage);
     }
 
     onLanguageChange(e: Event) {
@@ -30,6 +44,7 @@ export class LanguageSelectorComponent {
             throw new Error(`Expected e.target to be HTMLSelectElement, e was ${e?.constructor?.name ?? e}`);
         }
         this.selectedLanguage = e.target.value;
+        localStorage.setItem(LANG_KEY, this.selectedLanguage);
         this.translocoService.setActiveLang(this.selectedLanguage);
     }
 }
