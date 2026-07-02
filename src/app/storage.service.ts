f073f68db008153c4a98034bdaba8cd1f03a5795
Håkon Løvdal <kode@denkule.no>
2026-07-03 00:27:20 +0200

    Implement StorageService as a wrapper for localStorage

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -5,4 +5,16 @@
 import { Service } from "@angular/core";
 
 @Service()
-export class StorageService {}
+export class StorageService {
+  getItem(key: string): string | null {
+    return localStorage.getItem(key);
+  }
+
+  setItem(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+}
